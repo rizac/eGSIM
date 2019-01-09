fe1b8543e446471ccb4adf5eb68493b71ec209cd
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-01-09 21:44:47 +0100

    refactoring frontend

--- static/js/comps/trellis.js
+++ static/js/comps/trellis.js
@@ -10,7 +10,7 @@
 // the main <div> is the body of the <form>
 _TEMPLATE_TRELLIS = `
 <div class="d-flex flex-column flexible" slot-scope="self">
-    <div class="flexible d-flex flex-row">
+    <div class="flexible d-flex flex-row mb-4">
         <div class="flexible d-flex flex-column">
             
             <gsimselect :form="form" showfilter class="flexible mb-4"></gsimselect>
@@ -20,7 +20,7 @@ _TEMPLATE_TRELLIS = `
         
         <div class="d-flex flex-column flexible ml-4">
             <h5>Scenario configuration</h5>
-            <div class="flexible form-control" style="background-color:transparent">
+            <div class="flexible form-control mb-4" style="background-color:transparent">
 
                 <template v-for="(data, name) in self.form" v-if="!['gsim', 'imt', 'sa_periods', 'plot_type'].includes(name)">
                     <div class="d-flex flex-row mb-0 mt-2 pt-1 align-items-baseline">
@@ -42,30 +42,25 @@ _TEMPLATE_TRELLIS = `
                 </template>
                 
             </div>
+            
+            <template v-for="name in ['plot_type']">
+                <div class="d-flex flex-column">
+                    <div class='d-flex flex-row align-items-baseline'>
+                        <h5>{{ name }}</h5>
+                        <span class="text-danger small flexible ml-3 text-right">{{ self.form[name].err }}</span>
+                    </div>
+                    
+                    <select v-model="self.form[name].val" v-bind="self.form[name].attrs" size="4"  class='form-control'>
+                        <option v-for='opt in self.form[name].choices' :value="opt[0]">{{ opt[1] }}</option>
+                    </select>
+                </div>
+            </template>
         </div>
     </div>
 
-    <div class="d-flex flex-row mt-4">
-        
-        <template v-for="name in ['plot_type']">
-            <div class="flexible d-flex flex-column">
-                <div class='d-flex flex-row align-items-baseline'>
-                    <h5>{{ name }}</h5>
-                    <span class="text-danger small flexible ml-3 text-right">{{ self.form[name].err }}</span>
-                </div>
-                
-                <div class="d-flex flex-row flexible form-control" style="background-color:transparent">
-                    <label v-for='opt in self.form[name].choices' :for="'id_' + opt[0]" class='mr-3 text-nowrap'> 
-                        <input type='radio' v-model="self.form[name].val" :value="opt[0]" :id="'id_' + opt[0]"> {{ opt[1] }}
-                    </label> 
-                </div>
-            </div>
-        </template>
-
-        <button type="submit" class="btn btn-outline-primary ml-4">
-            Display plots
-        </button>
-    </div>
+    <button type="submit" class="btn btn-outline-primary">
+        Display plots
+    </button>
 
 </div>
 `
@@ -94,7 +89,7 @@ Vue.component('trellis', {
           });
       }
   },
-  template: `<egsimform style='max-width:70vw' :form='form' :url='url'
+  template: `<egsimform :form='form' :url='url'
               v-on:submit="request"
               :class="modal ? ['shadow', 'border'] : ''" :modal='modal' :hidden="hidden" class='align-self-center m-4'>
       ${_TEMPLATE_TRELLIS}
