feac1aef8ad716eaa8fed0c2d9e2a4c675151bda
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-04-08 21:30:50 +0200

    fixing stuff WIP

--- egsim/management/commands/initdb.py
+++ egsim/management/commands/initdb.py
@@ -14,22 +14,32 @@ import warnings
 import inspect
 from collections import OrderedDict
 from importlib import import_module
+import importlib.util
 
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand, CommandError
 from openquake.baselib.general import DeprecationWarning as OQDeprecationWarning
 from openquake.hazardlib.gsim.base import NotVerifiedWarning
 from openquake.hazardlib.gsim import get_available_gsims
 from openquake.hazardlib.imt import registry as hazardlib_imt_registry
 from openquake.hazardlib.const import TRT
 from openquake.hazardlib import imt
-from django.core.management.base import BaseCommand, CommandError
+
 
 from egsim.models import Gsim, Imt, Trt, Error, TectonicRegion, ENTITIES, \
     empty_all
-from django.db.utils import OperationalError
+from ._tectonic_regionalisations import SHARE
 
+# these are the functions to populate the TectonicRegion model.
+# For info on implementing new ones, see tectonic_regionalisations/README.txt
+# The key is the model name, the value is the relative function
+TECREG_FUNCTIONS = {'SHARE': SHARE.create}
 
 
 class Command(BaseCommand):
+    '''Command to initialize the db:
+        python manage.py initdb
+    '''
     help = ('Initializes the database and creates with OpenQuake information'
             'and tectonic regions. The latter can be incremented by writing'
             'a new folder in the `egsim/management/commands/tr_models`'
@@ -51,28 +61,57 @@ class Command(BaseCommand):
                                    'com/en/2.2/topics/migrations/#workflow)' %
                                    str(no_db))
 
-            # set data        
+            # set data:
             self.stdout.write('Writing OQ data (Trt, IMT, GSIMs)')
             trts, imts = get_trts(), get_imts()
             get_gsims(trts, imts)
 
-            tecregions = import_trs_functions()
             self.stdout.write('Writing Tectonic regions from provided models')
-            self.stdout.write('Found %d model(s): %s' % (len(tecregions),
-                                                         list(tecregions.keys())))
+            self.stdout.write('Found %d model(s): %s' %
+                              (len(TECREG_FUNCTIONS),
+                               list(TECREG_FUNCTIONS.keys())))
 
-            for model, func in tecregions:
+            for model, func in TECREG_FUNCTIONS.items():
                 tecregions = func(trts)
-                for t in tecregions:
-                    t.model = model
-                    t.save()
+                for trg in tecregions:
+                    trg.model = model
+                    trg.save()
 
-            self.stdout.write(self.style.SUCCESS('Successfully initialized the db'))
+            trts = Trt.objects.count()  # pylint: disable=no-member
+            imts = Imt.objects.count()  # pylint: disable=no-member
+            gsims = Gsim.objects.count()  # pylint: disable=no-member
+
+            errors = Error.objects  # pylint: disable=no-member
+            gsims_errs = errors.filter(entity_type=ENTITIES[0][0]).count()
+            imts_errs = errors.filter(entity_type=ENTITIES[1][0]).count()
+            trts_errs = errors.filter(entity_type=ENTITIES[2][0]).count()
+
+            self.stdout.write(self.style.SUCCESS('Successfully initialized '
+                                                 'the db:'))
+            self.stdout.write(self.style.SUCCESS('  %d Trt(s) written '
+                                                 '(%d skipped due to '
+                                                 'errors)' % (trts,
+                                                              trts_errs)))
+            self.stdout.write(self.style.SUCCESS('  %d Imt(s) written '
+                                                 '(%d skipped due to '
+                                                 'errors)' % (imts,
+                                                              imts_errs)))
+            self.stdout.write(self.style.SUCCESS('  %d Gsim(s) written '
+                                                 '(%d skipped due to '
+                                                 'errors)' % (gsims,
+                                                              gsims_errs)))
+            trs = TectonicRegion.objects  # pylint: disable=no-member
+            for key in TECREG_FUNCTIONS:
+                count = trs.filter(model=key).count()
+                self.stdout.write(self.style.SUCCESS('%d Tectonic region(s) '
+                                                     'written (model: %s)' %
+                                                     (count, key)))
+
         except CommandError:
             raise
         except Exception as exc:
             raise CommandError(exc)
-   
+
 
 def get_trts():
     '''Writes all Tectonic region types from OpenQuake into the db,
@@ -81,9 +120,8 @@ def get_trts():
     create_trt = Trt.objects.create  # pylint: disable=no-member
     for attname in dir(TRT):
         if attname[:1] != '_' and isinstance(getattr(TRT, attname), str):
-            trts.append(create_trt(key=getattr(TRT, attname).
-                                   replace(' ', '_').lower(),
-                                   oq_name=getattr(TRT, attname),
+            # the key is set automatically from the oq_name (see models.py):
+            trts.append(create_trt(oq_name=getattr(TRT, attname),
                                    oq_att=attname))
     return trts
 
@@ -91,11 +129,13 @@ def get_trts():
 def get_imts():
     '''Writes all IMTs from OpenQuake to the db, skipping IMTs which need
     arguments as we do not know how to handle them (except SA)'''
-    entyty_type = ENTITIES[1][0]
+    entity_type = ENTITIES[1][0]
     create_err = Error.objects.create  # pylint: disable=no-member
     create_imt = Imt.objects.create  # pylint: disable=no-member
     imts = []
     for imt_name, imt_class in hazardlib_imt_registry.items():
+        if inspect.isabstract(imt_class):
+            continue
         if imt_class != imt.SA:  # test we di not need arguments if imt is not SA
             try:
                 # this creates an instance of the class. If the instance needs argument,
@@ -104,15 +144,16 @@ def get_imts():
             except Exception as exc:  # pylint: disable=broad-except
                 create_err(type=exc.__class__.__name__,
                            message=str(exc),
-                           entyty_type=entyty_type,
-                           entity_name=imt_name)
+                           entity_type=entity_type,
+                           entity_key=imt_name)
                 continue
-            imts.append(create_imt(key=imt_name, needs_args=imt_class==imt.SA))
+        imts.append(create_imt(key=imt_name, needs_args=imt_class == imt.SA))
     return imts
 
 
 def get_gsims(trts, imts):
-    entyty_type = ENTITIES[0][0]
+    '''Writes all Gsims from OpenQuake to the db'''
+    entity_type = ENTITIES[0][0]
     trts_d = {_.oq_name: _ for _ in trts}
     imts_d = {_.key: _ for _ in imts}
     create_err = Error.objects.create  # pylint: disable=no-member
@@ -135,15 +176,15 @@ def get_gsims(trts, imts):
             except (OSError, NotImplementedError) as exc:
                 create_err(type=exc.__class__.__name__,
                            message=str(exc),
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
             except OQDeprecationWarning as warn:
                 # the builtin DeprecationWarning is silenced, OQ uses it's own
                 create_err(type=warn.__class__.__name__,
                            message=str(warn),
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
 #             except NotVerifiedWarning as warn:
 #                 warning = str(warn)
@@ -154,14 +195,14 @@ def get_gsims(trts, imts):
             except AttributeError as exc:
                 create_err(type=exc.__class__.__name__,
                            message=str(exc),
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
             if not gsim_imts and hasattr(gsim_imts, '__iter__'):
                 create_err(type=Exception.__name__,
                            message='No IMT defined',
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
             try:
                 trt = trts_d[gsim_inst.DEFINED_FOR_TECTONIC_REGION_TYPE]
@@ -169,41 +210,28 @@ def get_gsims(trts, imts):
                 create_err(type=Exception.__name__,
                            message='%s is not a valid TRT' %
                            str(gsim_inst.DEFINED_FOR_TECTONIC_REGION_TYPE),
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
             except AttributeError:
                 create_err(type=Exception.__name__,
                            message='No TRT defined',
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
-            gimts = [imts_d[_] for _ in gsim_imts if _ in imts_d]
-            if not gimts:
+            # convert gsim imts (classes) into strings:
+            gsim_imts = [_.__name__ for _ in gsim_imts]
+            # and not convert to Imt model instances:
+            gsim_imts = [imts_d[_] for _ in gsim_imts if _ in imts_d]
+            if not gsim_imts:
                 create_err(type=Exception.__name__,
                            message='No IMT in %s' % str([_.key for _ in imts]),
-                           entyty_type=entyty_type,
-                           entity_name=key)
+                           entity_type=entity_type,
+                           entity_key=key)
                 continue
             gsim = create_gsim(key=key, trt=trt, warning=warning,
                                needs_args=needs_args)
-            gsim.set(gimts)
+            gsim.imts.set(gsim_imts)
             gsims.append(gsim)
 
     return gsims
-
-
-def import_trs_functions():
-    root = os.path.join(os.path.dirname(__file__),
-                        'tectonic_regionalisations')
-    ret = {}
-    for model in os.listdir(root):        
-        mod = import_module(os.path.join(root, model))
-        met = getattr(mod, 'create')
-        if hasattr(met, '__call__'):
-            ret[model] = met
-
-    return ret
-    
-
-
--- tests/client/test_gsims.py
+++ tests/client/test_gsims.py
@@ -5,15 +5,19 @@ Created on 22 Oct 2018
 
 @author: riccardo
 '''
+import pytest
+
 from egsim.core.utils import querystring, EGSIM
+from egsim.models import aval_gsims
 
 
+@pytest.mark.django_db
 class Test:
 
     url = '/query/gsims'
     request_filename = 'request_gsims.yaml'
 
-    def test_gsims_service(self, testdata, areequal, client):
+    def test_gsims_service(self, testdata, areequal, django_db_setup, client):
         '''tests the gsims API service.
         :param requesthandler: pytest fixture which handles the read from the yaml file
             request.yaml which MUST  BE INSIDE this module's directory
@@ -26,14 +30,26 @@ class Test:
         inputdic = testdata.readyaml(self.request_filename)
         resp1 = client.get(querystring(inputdic, baseurl=self.url))
         resp2 = client.get(self.url, data=inputdic)
-
         assert resp1.status_code == resp2.status_code == 200
         assert areequal(resp1.json(), resp2.json())
 
-        expected_gsims = [k for k, v in EGSIM.aval_gsims.items() if v.trt == inputdic['trt']]
+        expected_gsims = [k[0] for k in aval_gsims(asjsonlist=True)
+                          if k[2] == inputdic['trt']]
         assert sorted(resp1.json()) == sorted(expected_gsims)
 
-    def test_gsims_service_no_result_wrong_trt(self, testdata, areequal, client):
+        # now try to supply two filters on gsim:
+        expected_gsims = [_ for _ in expected_gsims if _[0] in ('a', 'A')]
+        inputdic['gsim'] = expected_gsims
+        resp1 = client.get(self.url, data=inputdic)
+        inputdic['gsim'] = 'a*'
+        resp2 = client.get(self.url, data=inputdic)
+        assert resp1.status_code == resp2.status_code == 200
+        assert areequal(resp1.json(), resp2.json())
+        assert sorted(resp1.json()) == sorted(resp1.json()) == \
+            sorted(expected_gsims)
+
+    def test_gsims_service_no_result_wrong_trt(self, testdata, areequal,
+                                               django_db_setup, client):
         '''tests the gsims API service.
         :param requesthandler: pytest fixture which handles the read from the yaml file
             request.yaml which MUST  BE INSIDE this module's directory
@@ -45,17 +61,15 @@ class Test:
         '''
         inputdic = testdata.readyaml(self.request_filename)
         inputdic.update(trt='stable_continental_region')
-
         # use a key which is not in the defined sets of OpenQuake's TRTs:
         resp1 = client.get(querystring(inputdic, baseurl=self.url))
         resp2 = client.get(self.url, data=inputdic)
-
         assert resp1.status_code == resp2.status_code == 400
-
         assert areequal(resp1.json(), resp2.json())
         assert isinstance(resp1.json(), dict) and resp1.json()['error']['code'] == 400
 
-    def test_gsims_service_imt_no_match(self, testdata, areequal, client):
+    def test_gsims_service_imt_no_match(self, testdata, areequal,
+                                        django_db_setup, client):
         '''tests the gsims API service.
         :param requesthandler: pytest fixture which handles the read from the yaml file
             request.yaml which MUST  BE INSIDE this module's directory
@@ -66,14 +80,13 @@ class Test:
             function is iteratively called with a list of different Clients created in conftest.py
         '''
         inputdic = testdata.readyaml(self.request_filename)
-        expected_gsims = [k for k, v in EGSIM.aval_gsims.items() if v.trt == inputdic['trt']]
-        # use a key which is not in the defined sets of OpenQuake's TRTs.
+        # use a key which is not in the defined sets of OpenQuake's IMTs.
         inputdic.update(imt='*KW?,[]')
         resp1 = client.get(querystring(inputdic, baseurl=self.url))
-        # use another key which is not in the defined sets of OpenQuake's TRTs:
+        # use another key which is not in the defined sets of OpenQuake's IMTs:
         inputdic.update(imt='KW?')
         resp2 = client.get(querystring(inputdic, baseurl=self.url))
-    
+
         assert areequal(resp1.json(), resp2.json())
         # no gsim found:
         assert resp1.status_code == resp2.status_code == 200
