d56671d51b6cc345ef3d60e40f55f3bfb4b21f65
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-04-15 23:53:08 +0200

    implementing forminput in frontend

---
+++ static/js/comps/base/forminput.js
@@ -0,0 +1,39 @@
+/**
+ * Implements a simple form input (or radio, or checkbox) from a given Object holding
+ * the component properties and a given name
+ */
+Vue.component('forminput', {
+    //https://vuejs.org/v2/guide/components-props.html#Prop-Types:
+    props: {
+        'data': {type: Object},  // an object with properties 'err', 'val', 'choices' ...
+        'name': {type: String}
+    },
+    data: function () {
+        return {
+            // no-op for the moment
+        }
+    },
+    template: `<div>
+        <div class="d-flex flex-row mb-0 pt-1 align-items-baseline">
+            <label :for="data.attrs.id" class='mb-0 text-nowrap'>
+                <input v-if="!data.choices.length && ['radio', 'checkbox'].includes(data.attrs.type)"
+                    v-model="data.val" v-bind="data.attrs" class='mr-1'>
+                {{ name }}
+            </label>
+            <div class="text-muted small flexible ml-3 text-right">
+                <span v-if="data.err" class="text-danger">{{ data.err }}</span>
+                <span v-if="!data.err && data.label.toLowerCase() != name.toLowerCase() && data.help"
+                    v-html="data.label + ' (' + data.help + ')'"></span>
+                <span v-if="!data.err && data.label.toLowerCase() != name.toLowerCase() && !data.help"
+                    v-html="data.label"></span>
+                <span v-if="!data.err && data.label.toLowerCase() == name.toLowerCase() && data.help"
+                    v-html="data.help"></span>
+            </div>
+        </div>
+        <input v-if="!data.choices.length && !['radio', 'checkbox'].includes(data.attrs.type)"
+            v-model="data.val" v-bind="data.attrs" class='form-control'>
+        <select v-if="data.choices.length" v-model="data.val" v-bind="data.attrs" class='form-control'>
+            <option v-for='opt in data.choices' :value='opt[0]'>{{ opt[1] }}</option>
+        </select>
+    </div>`
+})
--- static/js/comps/gmdbplot.js
+++ static/js/comps/gmdbplot.js
@@ -33,27 +33,14 @@ Vue.component('gmdbplot', {
         <div>
             <form novalidate v-on:submit.prevent="request" class='d-flex flex-column'>
                 <div class="d-flex flex-column flexible">
-                    <template v-for="(data, name) in form">
-                        <div class="d-flex flex-row mb-0 mt-2 pt-1 align-items-baseline">
-                            <label :for="data.attrs.id" class='mb-0 text-nowrap'>
-                                <input v-if="!data.choices.length && ['radio', 'checkbox'].includes(data.attrs.type)" v-model="data.val" v-bind="data.attrs" class='mr-1'>
-                                {{ name }}
-                            </label>
-                            <div class="text-muted small flexible ml-3 text-right">
-                                <span v-if="data.err" class="text-danger">{{ data.err }}</span>
-                                <span v-if="!data.err && data.label.toLowerCase() != name.toLowerCase() && data.help" v-html="data.label + ' (' + data.help + ')'"></span>
-                                <span v-if="!data.err && data.label.toLowerCase() != name.toLowerCase() && !data.help" v-html="data.label"></span>
-                                <span v-if="!data.err && data.label.toLowerCase() == name.toLowerCase() && data.help" v-html="data.help"></span>
-                            </div>
-                        </div>
-                        <input v-if="!data.choices.length && !['radio', 'checkbox'].includes(data.attrs.type)" v-model="data.val" v-bind="data.attrs" class='form-control'>
-                        <select v-if="data.choices.length" v-model="data.val" v-bind="data.attrs" class='form-control'>
-                            <option v-for='opt in data.choices' :value='opt[0]'>{{ opt[1] }}</option>
-                        </select>
-                    </template>
+
+                    <forminput v-for="(data, name) in form" :key="name" :data='data' :name='name'>
+                    </forminput>
+
                     <button type="submit" class="btn btn-outline-primary mt-2">
                         Display magnitude-distance plot
                     </button>
+
                 </div>
             </form>
         </div>
