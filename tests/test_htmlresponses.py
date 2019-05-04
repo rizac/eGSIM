7b36ef1beb28e1d07009a3d01da4ca8e47d98cdc
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-05-04 21:32:03 +0200

    improves apidoc tests

--- tests/test_htmlresponses.py
+++ tests/test_htmlresponses.py
@@ -1,5 +1,5 @@
 '''
-Tests the eGSIM Django commands
+Tests the eGSIM Django http responses (HTTP pags)
 
 Created on 6 Apr 2019
 
@@ -13,16 +13,18 @@ from mock import patch
 
 @pytest.mark.django_db
 def test_apidoc(client):
-    '''Test gmdb_esm command (and consequently, also gmbd command)'''
+    '''Test apidoc page generation'''
     result = client.get('/pages/apidoc')
     assert result.status_code == 200
     # stupid assert (better than nothing for the moment):
     assert b"[eGSIM domain URL]/query" in result.content
+    # check we do not have django undefined variables:
+    assert b" NOT FOUND" not in result.content
 
 
 @pytest.mark.django_db
 def test_main(client):
-    '''Test gmdb_esm command (and consequently, also gmbd command)'''
+    '''Test main page generation'''
     result = client.get('/')
     assert result.status_code == 302
     # stupid assert (better than nothing for the moment):
@@ -37,6 +39,6 @@ def test_main(client):
 
 @pytest.mark.django_db
 def test_home(client):
-    '''Test gmdb_esm command (and consequently, also gmbd command)'''
+    '''Test home page generation'''
     result = client.get('/pages/home')
     assert result.status_code == 200
