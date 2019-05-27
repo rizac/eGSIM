578a4d4d8a5e43f88becc79978030e5c31175818
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-05-27 16:52:53 +0200

    simplifying urls and views, adding responseerrors and removing middleware

--- egsim/settings.py
+++ egsim/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'egsim.middlewares.ExceptionHandlerMiddleware'
+    # 'egsim.middlewares.ExceptionHandlerMiddleware'
 ]
 
 ROOT_URLCONF = 'egsim.urls'
