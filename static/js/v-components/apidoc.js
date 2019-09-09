9d91d87b474836c887252d881baced794634952a
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-09-09 17:13:07 +0200

    fixing frontend css

--- static/js/v-components/apidoc.js
+++ static/js/v-components/apidoc.js
@@ -9,33 +9,5 @@ Vue.component('apidoc', {
     	id: {String, default: '__apidoc__iframe__'},
     	fragment: {String, default: ''}
     },
-    template: `<iframe :id='id' class='flexible' :src='src + fragment'></iframe>`,
-    /*methods: {
-    	getSrc(){
-    		// return this.src unless fragments are given in the url
-    		// in case, pass the fragment to the iframe:
-            var pathname = window.location.href.split('#');
-            if (pathname.length > 1){
-		    	var fragment = pathname.pop();
-		    	if (!fragment.includes('/')){  // brutal check for safety ...
-		    		return `${this.src}#${fragment}`
-		    	}
-            }
-			return this.src;
-        }
-    },
-    mounted: function(){
-    	// listen for changes of fragment from within the iframe:
-    	var iframe = document.getElementById(this.id);
-    	if (iframe){
-	    	var me = this;
-	    	iframe.contentWindow.onhashchange = function(evt){
-	    		var oldUrl = evt.oldURL;
-	    		var newUrl = evt.newURL;
-	    		if (newUrl.startsWith(oldUrl + '#')){
-	    			me.fragment = newUrl.split('#').pop();
-	    		}
-	    	}
-	   	}
-    }*/
+    template: `<iframe :id='id' class='flexible' :src='src + fragment'></iframe>`
 })
