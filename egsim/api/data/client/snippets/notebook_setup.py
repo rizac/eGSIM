c89350d40149d0e9f6696f493b0986ff138fea04
rizac <riccardo.zaccarelli@gmail.com>
2024-01-30 10:20:22 +0100

    simplify notebooks rm notebok setup file

--- egsim/api/data/client/snippets/notebook_setup.py
+++
@@ -1,9 +0,0 @@
-# IPython/Jupyter setup (Optional: edit or remove at your wish)
-%reload_ext autoreload
-%autoreload 2
-from IPython.display import display, HTML, Markdown
-# make wide HTML cells (e.g., as created by `display(dataframe)`) display a scrollbar:
-display(HTML("<style>div.jp-OutputArea-output.jp-RenderedHTML{display:block;overflow:auto;}</style>"))
-from IPython.core.display import HTML
-# make tables cells with a border:
-display(HTML("<style>th, td{border: 1px solid #DDE !important;}</style>"))
