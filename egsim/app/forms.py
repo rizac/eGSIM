dfaf653c1753f1725f14939af2ceed4b55681b66
rizac <riccardo.zaccarelli@gmail.com>
2024-03-06 17:56:41 +0100

    residuals plots completed

--- egsim/app/forms.py
+++ egsim/app/forms.py
@@ -1,4 +1,6 @@
 """Forms for the eGSIM app (web portal)"""
+import numpy as np
+
 from egsim.api.forms.flatfile.management import Plotly
 from egsim.api.forms.flatfile.residuals import ResidualsForm
 from egsim.api.forms.predictions import PredictionsForm
@@ -27,6 +29,12 @@ class ResidualsPlotDataForm(ResidualsForm):
         # likelihood (x y): Frequency LH(<imt>)
         # <ff_column> (x y): Z(<imt>) <ff_column>
 
+        # remember: on the frontend:
+        # - to enable the xaxis type checkbox: layoyt.xaxis.type must be 'log' or 'linear'
+        # (same for layout.yaxis)
+        # - to enable the xaxis sameRange checkbox: layout.xaxis.range must be defined
+        # as 2-element list (same for yaxis)
+        # Plotly.get_layout (see below) handles this automatically
         df_labels = {
             c_labels.total_res: 'Total',
             c_labels.intra_ev_res: 'Intra event',
@@ -50,12 +58,6 @@ class ResidualsPlotDataForm(ResidualsForm):
         else:
             y_label = lambda imt: f'Z ({str(imt)})'
             x_label = lambda imt: self.cleaned_data['x']
-            # Set col_x as tuple (real dataframe column).
-            # Not really elegant how to retrieve x, however:
-            # for c in dataframe.columns:
-            #     if c[0] == c_labels.input_data and c[-1] == col_x:
-            #         col_x = c
-            #         break
 
         plots = []
         colors_cycle = Plotly.colors_cycle()
@@ -63,44 +65,88 @@ class ResidualsPlotDataForm(ResidualsForm):
         for col in [c for c in dataframe.columns if c[1] in df_labels]:
             imt, res_type, model = col
             color = colors.setdefault(model, next(colors_cycle))
-            default_trace = {
-                'marker': {
-                    'color': color.replace(', 1)', ', 0.5)') # make semi transparent
-                },
-                'name': " ".join(col),
-                'legendgroup': model
-            }
-
             default_layout = lambda: {
                 'xaxis': {'title': x_label(imt)},
                 'yaxis': {'title': y_label(imt)}
             }
 
             if not col_x:
-                default_trace['xbins'] = { 'size': .5 }
-                default_trace['histnorm'] = 'probability'
-                default_trace['marker']['line'] = {
-                    'color': color,
-                    'width': 2
-                }
                 x = dataframe[col]
-                y = None
-                trace = Plotly.get_trace(x=x, **default_trace)
+                if not likelihood:
+                    step = 0.5
+                else:
+                    step = 0.1
+                bins = np.arange(x.min(), x.max() + step, step)
+                y = np.histogram(x, bins, density=True)[0]
+                trace = {
+                    'x': Plotly.array2json(bins[:-1]),
+                    'y': Plotly.array2json(y),
+                    'type': 'bar',
+                    'marker': {
+                        'color': color.replace(', 1)', ', 0.5)'),
+                        'line': {
+                            'color': color,
+                            'width': 2
+                        }
+                    },
+                    'name': " ".join(col),
+                    'legendgroup': model
+                }
                 data = [trace]
+
+                if not likelihood:
+                    mean, std = x.mean(), x.std()
+                    x_ = np.arange(mean - 3 * std, mean + 3 * std, step / 10)
+                    data.append({
+                        'x': Plotly.array2json(x_),
+                        'y': Plotly.array2json(norm_dist(x_, mean, std)),
+                        'type': 'scatter',
+                        'mode': 'lines',
+                        'line': {
+                            'color': color,
+                            'width': 2
+                        },
+                        'name': " ".join(col) + ' normal distribution',
+                        'legendgroup': model + ' normal distribution',
+                    })
+                    data.append({
+                        'x': Plotly.array2json(x_),
+                        'y': Plotly.array2json(norm_dist(x_)),
+                        'type': 'scatter',
+                        'mode': 'lines',
+                        'line': {
+                            'color': "rgba(120, 120, 120, 1)",
+                            'width': 2,
+                            'dash': 'dot'
+                        },
+                        'name': 'Normal distribution',
+                        'legendgroup': 'Normal distribution (m=0, s=1)',
+                    })
                 def_layout = default_layout()
-                def_layout['xaxis']['type'] = '-'
-                def_layout['yaxis']['type'] = Plotly.AxisType.linear
-                def_layout['yaxis']['range'] = [0, 1]
-                # layout['xaxis']['type'] = Plotly.AxisType.infer  # disables log axis control
             else:
-                default_trace['mode'] = 'markers'
-                default_trace['marker']['size'] = 10
                 x = self.cleaned_data['flatfile'][col_x]
                 y = dataframe[col]
-                trace = Plotly.get_trace(x=x, y=y, **default_trace)
+                trace = {
+                    'x': Plotly.array2json(x),
+                    'y': Plotly.array2json(y),
+                    'type': 'scatter',
+                    'mode': 'markers',
+                    'marker': {
+                        'color': color.replace(', 1)', ', 0.5)'),
+                        'line': {
+                            'color': color,
+                            'width': 2
+                        }
+                    },
+                    'name': " ".join(col),
+                    'legendgroup': model
+                }
                 data = [trace]
                 def_layout = default_layout()
 
+            # config layout based on the displayed data x, y. If not already set,
+            # layout['xaxis']['type'] and layout['xaxis']['range'] will be inferred from
+            # x (same for y and layout['xaxis']):
             layout = Plotly.get_layout(x = x, y = y, **def_layout)
 
             plots.append({
@@ -113,7 +159,11 @@ class ResidualsPlotDataForm(ResidualsForm):
                 'layout': layout
             })
 
-        Plotly.harmonize_axis_ranges([p['layout']['xaxis'] for p in plots])
-        Plotly.harmonize_axis_ranges([p['layout']['yaxis'] for p in plots])
         return {'plots': plots}
 
+
+def norm_dist(x, mean=0, sigma=1):
+    from scipy.constants import pi
+    sigma_square_times_two = 2 * (sigma ** 2)
+    norm = 1. / np.sqrt(2 * pi * sigma_square_times_two)
+    return norm * np.exp(-((x - mean) ** 2) / sigma_square_times_two)
