b9bbc9d72d67abc571006cffcbb1854d19cffd08
rizac <riccardo.zaccarelli@gmail.com>
2025-03-03 12:19:41 +0100

    add pandas in setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     # FOR DEV/TESTS, add: `pip install pytest`
     install_requires=[
         'openquake.engine==3.15.0',  # 1st tested version was >3.5.0
+        'pandas>=2.2.2',
         'pyyaml>=6.0',
         'tables>=3.8.0',
     ],
