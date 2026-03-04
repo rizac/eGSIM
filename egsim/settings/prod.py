36b1d4a7e00652eb86cdfc4de6779d6244f71f31
rizac <riccardo.zaccarelli@gmail.com>
2026-03-04 12:52:22 +0100

    settings file in production via osenv

--- egsim/settings/prod.py
+++ egsim/settings/prod.py
@@ -1,11 +1,12 @@
 """
-Django settings TEMPLATE for production.
-Copy / paste it in a secure server location
-and edit missing data (see declared variables below)
-As convention, you may rename the production file `settings.py`
+Django settings for production.
+
+Configure required environment variables in your deployment environment (e.g. service
+file)
 """
 
 from egsim.settings.base import *  # Note: you also import pathlib.Path automatically
+import os
 
 DEBUG = False  # DO NOT CHANGE THIS IN PRODUCTION!
 
@@ -21,16 +22,18 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECRET_KEY SHOULD BE UNIQUE FOR EACH SETTINGS FILE AND CAN BE GENERATED ON THE
 # TERMINAL (WITHIN THE DJANGO VIRTUAL ENV)  WITH THE COMMAND:
 # python -c "from django.core.management.utils import get_random_secret_key;print(get_random_secret_key())"  # noqa
-SECRET_KEY: str
+SECRET_KEY = os.environ["EGSIM_SECRET_KEY"]
+
+__DATA_ROOT__ = Path(os.environ['EGSIM_DATA_ROOT'])
 
 # path to the sqlite database (you may place it next to MEDIA_ROOT)
-DATABASES['default']['NAME']: str
+DATABASES['default']['NAME'] = __DATA_ROOT__ / 'db.sqlite'
 
 # media root (path on the server):
-MEDIA_ROOT: str
+MEDIA_ROOT = __DATA_ROOT__ / 'media'
 
 # static files root (path on the server)
-STATIC_ROOT: str
+STATIC_ROOT = __DATA_ROOT__ / 'staticfiles'
 
 
 
